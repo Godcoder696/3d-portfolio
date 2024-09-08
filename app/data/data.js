7780003d772485492a5022972ee919d1305d8966
Godcoder696 <117354227+Godcoder696@users.noreply.github.com>
2024-09-08 22:31:44 +0530

    new project added

--- app/data/data.js
+++ app/data/data.js
@@ -1,5 +1,13 @@
 export const projects=[
     {
+      title:"SMART WASTE SEGREGATOR",
+      mobile: "A solution providing effective segregation of dry and wet waste precisely using moisture levels as primary parameters to achieve the same. The web based console provides login, insights if the individual dustbins, analytics & route optimization feature to effectively collect and dispose waste with a optimized route to mavigate through.",
+      desktop: "A solution for precise segregation of dry and wet waste based on moisture levels is implemented through smart bins. A web-based console offers login access, real-time insights into individual dustbins, and advanced analytics. It features a route optimization tool to efficiently plan waste collection routes, reducing time and fuel consumption. This system enhances waste management by improving segregation accuracy and streamlining the disposal process through optimized navigation for collection vehicles.",
+      url:"/SWS.png",
+      github:"https://github.com/Godcoder696/smart-waste-management",
+      website:null
+    },
+    {
       title:"MOMENTUM 2023",
       mobile: "A college fest site for event registration and showcasing the 2023 theme 'NOVA NEXUS' on sustainability. Uses Figma for design, Express.js for backend, deployed on DigitalOcean, with RazorPay and NodeMailer integration.",
       desktop: "A College fest website used for registering for events, seeing events and displaying the theme for the 2023 year 'NOVA NEXUS'. Which symbolizes sustainability via modern norms. Designed the theme on sustainable development using Figma Designs.Established express server for backend and frontend interaction. Deploying and resolving errors on the platform digital ocean.Integrated RazorPay with application for error free, smooth and seemless transactions. Implemented automatic mail system which gives the registrations data in every 24 hours to the collegeusing Node-mailer.",
@@ -8,20 +16,20 @@ export const projects=[
       website:null
     },
     {
-      title:"AJANTA SIRSA",
-      mobile: "A modern portfolio site for Ajanta Clothouse, Sirsa, featuring products and services. Uses MongoDB Atlas, React.js with Chakra UI, and Node.js with Express.js for seamless RESTful API interactions.",
-      desktop: "A personalised portfolio website forAjanta Clothouse, Sirsa. The website displays the products, services and options they offer in a creative way of modern world. The website allows the shopkeeper to stand up with his own identity with meeting the customer audience. The database used is MongoDB Atlas. Which stores media in form base64 encryption. Frontend uses Chakra UI, React js & other react libraries like react-icons, etc.Backend uses Node js and http/https server Express js for seemless RESTfull API interactions.The project as a whole contains 2 microservices ie backend & frontend.",
-      url:"https://res.cloudinary.com/dsnfwiuid/image/upload/v1721201359/ajanta_fcyxno.png",
-      github: "https://github.com/Godcoder696/Ajanta-",
-      website:"https://ajantasirsa.com/"
-    },
-    {
       title:"CONNECTIVE",
       mobile: "A chat app for one-on-one and group chats using socket.io. Frontend uses Chakra UI and Context API, while backend uses Express.js, Node.js, and MongoDB for managing routes, chats, and user data.",
       desktop: "A chat application which is used for one to one and group chat by utilizing socket.io. Frontend is implemented using chakra UI which further uses Context API for managing global states of the client. Backend includes server made from express js for managing routes, Node js for writing server side script and Mongo DB for storing and managing chats and users data.",
       url:"https://res.cloudinary.com/dsnfwiuid/image/upload/v1721201359/connective_cqlls0.png",
       github:"https://github.com/Godcoder696/Connective",
       website:"https://connective-vhxb.onrender.com/"
+    },
+    {
+      title:"AJANTA SIRSA",
+      mobile: "A modern portfolio site for Ajanta Clothouse, Sirsa, featuring products and services. Uses MongoDB Atlas, React.js with Chakra UI, and Node.js with Express.js for seamless RESTful API interactions.",
+      desktop: "A personalised portfolio website forAjanta Clothouse, Sirsa. The website displays the products, services and options they offer in a creative way of modern world. The website allows the shopkeeper to stand up with his own identity with meeting the customer audience. The database used is MongoDB Atlas. Which stores media in form base64 encryption. Frontend uses Chakra UI, React js & other react libraries like react-icons, etc.Backend uses Node js and http/https server Express js for seemless RESTfull API interactions.The project as a whole contains 2 microservices ie backend & frontend.",
+      url:"https://res.cloudinary.com/dsnfwiuid/image/upload/v1721201359/ajanta_fcyxno.png",
+      github: "https://github.com/Godcoder696/Ajanta-",
+      website:"https://ajantasirsa.com/"
     }
 ]
 
